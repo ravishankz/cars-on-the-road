ec43f6ef980f486e66628d764e4c162df5642530
ravishankz <ravishankz@users.noreply.github.com>
2024-03-19 17:56:28 +0530

    car turn buttons added.

--- app.js
+++ app.js
@@ -13,12 +13,19 @@ let roadLineVLeft, roadLineVRight;
 let roadLinesHLeft = [];
 let roadLinesHRight = [];
 
+let carPosition;
+let carLVelocity, carRVelocity;
+
 function setup() {
     createCanvas(canvasWidth * canvasSizeMultiplier, canvasHeight * canvasSizeMultiplier);
     
     carWidth = width / 8;
     carHeight = height / 8;
     carCornerRadius = width / 40;
+
+    carPosition = createVector(width / 2, height / 4 * 3);
+    carLVelocity = createVector(-20, 0);
+    carRVelocity = createVector(20, 0);
     
     roadLineVWidth = width / 20;
     roadLineHHeight = height / 20;
@@ -40,13 +47,6 @@ function setup() {
 function draw() {
     background(roadColor);
     
-    // CAR
-    rectMode(CENTER);
-    stroke(86, 173, 0);
-    strokeWeight(width / 100);
-    fill(167, 255, 79);
-    rect(width / 2, height / 4 * 3, carWidth, carHeight, carCornerRadius);
-
     roadLineVLeft.display();
     roadLineVRight.display();
     
@@ -57,5 +57,27 @@ function draw() {
     for (let i = 0; i < 6; i++){
       roadLinesHRight[i].display();
     }  
+
+    // CAR
+    rectMode(CENTER);
+    stroke(86, 173, 0);
+    strokeWeight(width / 100);
+    fill(167, 255, 79);
+    rect(carPosition.x, carPosition.y, carWidth, carHeight, carCornerRadius);
 }
-  
+
+function keyPressed() {
+  if (keyCode === LEFT_ARROW) {
+    turnLeft();
+  } else if (keyCode === RIGHT_ARROW) {
+    turnRight();
+  }
+}
+
+function turnLeft() {
+  carPosition.add(carLVelocity);
+}
+
+function turnRight() {
+  carPosition.add(carRVelocity);
+}
